d6ae862988df6516349c99bad8b56e9b96ad8df2
Esteban <stbancr@hotmail.com>
2021-03-23 16:22:03 +0000

    Adding Fetch API

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 //include images into your bundle
@@ -7,28 +7,68 @@ import "bootstrap";
 
 //create your first component
 export function Home() {
-	let [todos, setTodos] = useState([]);
-	let [todo, setTodo] = useState("");
+	let [todosFetch, setTodosFetch] = useState([]);
+	let [input, setInput] = useState("");
 
-	const addTodo = todo => {
-		if (todo === "") {
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/emendez", {
+			method: "GET",
+			//body: JSON.stringify(data),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok);
+				console.log(resp.status);
+				return resp.json();
+			})
+			.then(data => {
+				setTodosFetch(data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}, []);
+
+	const addInput = input => {
+		if (input === "") {
 			window.alert("Add text to your To-Do");
 		} else {
-			setTodos([...todos, todo]);
+			setTodosFetch([...todosFetch, { label: input, done: false }]);
 		}
+		//setTodosFetch(newArray);
 	};
 
-	const add = todo => {
-		setTodo(todo);
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/emendez", {
+			method: "PUT", // or 'POST'
+			body: JSON.stringify(todosFetch), // data can be `string` or {object}!
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.then(response => console.log("Success:", JSON.stringify(response)))
+			.catch(error => console.error("Error:", error));
+	}, [todosFetch]);
+
+	const add = input => {
+		setInput(input);
 	};
 	const reset = () => {
 		document.querySelector("input").value = "";
-		setTodo("");
+		setInput("");
 	};
-	const deleteTodo = todoIndex => {
-		const newTodos = todos.filter((_, index) => index !== todoIndex);
-
-		setTodos(newTodos);
+	const deleteInput = inputIndex => {
+		const newTodos = todosFetch.filter((_, index) => index !== inputIndex);
+		setTodosFetch(newTodos);
+	};
+	const deleteAllItems = todosFetch => {
+		setTodosFetch([
+			{ label: "", done: false },
+			{ label: "", done: false }
+		]);
 	};
 	return (
 		<div className="d-flex justify-content-center text-center input-group mt-5 flex-column container-fluid w-50 alert alert-dark">
@@ -48,24 +88,35 @@ export function Home() {
 					className="btn btn-success"
 					id="submit"
 					onClick={() => {
-						addTodo(todo);
+						addInput(input);
 						reset();
 					}}
 					placeholder="Enter to-do">
 					Add
 				</button>
+				<button
+					className="btn btn-warning"
+					id="submit"
+					onClick={() => {
+						deleteAllItems();
+					}}
+					placeholder="Enter to-do">
+					Delete All
+				</button>
 			</div>
 
 			<div>
-				{todos.map((element, index) => (
+				{todosFetch.map((element, index) => (
 					<div className="container-fluid" key={index}>
 						<div className="alert alert-light row justify-content-center mw-100">
-							<div className="col-lg">{element}</div>
+							<div key={index} className="col-lg">
+								{element.label}
+							</div>
 							<div className="col-sm text-muted">
 								<i
 									className="fas fa-backspace"
 									onClick={() => {
-										deleteTodo(index);
+										deleteInput(index);
 									}}></i>
 							</div>
 						</div>
